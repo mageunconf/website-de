82d32d8167ad62f3eec192e0c622c088efc0b68a
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2019-11-12 22:18:40 +0100

    Update options.php

--- .options.php
+++ .options.php
@@ -3,11 +3,11 @@
 $lines = file('.options');
 
 $csvHeader = [];
-$csvData = [];
+$csvData   = [];
 // collect csvHeader
 
 foreach ($lines as $line) {
-    $data = unserialize($line);
+    $data = unserialize(trim($line));
     foreach ($data['options'] as $o) {
         $csvHeader[$o['label']] = $o['label'];
     }
@@ -15,22 +15,22 @@ foreach ($lines as $line) {
 ksort($csvHeader);
 // make data csv
 foreach ($lines as $line) {
-    $data = unserialize($line);
+    $data           = unserialize($line);
     $csvDataOneLine = array_combine($csvHeader, array_fill(0, count($csvHeader), ''));
 
     if (!isset($data['options'])) {
         continue;
     }
-    echo "\n\n";
+    #echo "\n\n";
     foreach ($data['options'] as $option) {
         $csvDataOneLine[$option['label']] = $option['value'];
-        var_dump($option['label']);
+        #var_dump($option['label']);
     }
     ksort($csvDataOneLine);
     $csvData[] = $csvDataOneLine;
 }
-
-$handle = fopen('.shirts.csv', 'w');
+$time = microtime(true);
+$handle = fopen(".shirts-$time.csv", 'w');
 fputcsv($handle, $csvHeader);
 foreach ($csvData as $line) {
     fputcsv($handle, $line);
--- .registrations.sql
+++ .registrations.sql
@@ -6,3 +6,6 @@ GROUP BY base_price_incl_tax - i.discount_amount;
 SELECT product_options FROM sales_flat_order_item i
 INNER JOIN sales_flat_order o ON o.entity_id = i.order_id
 WHERE status = 'complete' AND sku = 'LEJ-4';
+
+SELECT product_options FROM sales_flat_order_item oi INNER JOIN sales_flat_order o ON o.entity_id = oi.order_id
+WHERE sku IN ('mageunconf-2018-2', 'mageunconf-2018') AND status = 'complete'
